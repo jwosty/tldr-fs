7a3486ca7e7b9612e094c071875c277078eb57e2
jwosty <jwosty@users.noreply.github.com>
2018-04-24 19:28:43 -0500

    List packages

--- tldr-fs/Program.fs
+++ tldr-fs/Program.fs
@@ -1,12 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open FSharp.Data
+﻿open FSharp.Data
 open System
+open System.Data
+
+module String =
+    let join (separator: string) (strings: string seq) =
+        String.Join (separator, strings)    
+
+type Packages = JsonProvider<"https://tldr.sh/assets/index.json">
+
+let listAllPackages = async {
+    let! packages = Packages.AsyncGetSample()
+    for command in packages.Commands do
+        match command.Name.String with
+        | Some command -> printf "%s, " command
+        | None -> printf "(unknown value: '%A'), " command.Name.JsonValue
+    Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop)
+    printfn "  " }
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    //let index = Http.AsyncRequestString "http://tldr.sh/assets/index.json" |> Async.RunSynchronously
-    let index = JsonValue.Parse "[1,2,3]"
-    printfn "%A" index
+let main _ =
+    Async.RunSynchronously listAllPackages
     0
