c3c924739830da3e1a6dab27d6c43382c8ac34a3
jwosty <jwosty@users.noreply.github.com>
2018-04-24 20:33:37 -0500

    Checkin

--- tldr-fs/Program.fs
+++ tldr-fs/Program.fs
@@ -3,22 +3,23 @@ open FSharp.Data
 open System
 
 module String =
-    let join (separator: string) (strings: string seq) =
-        String.Join (separator, strings)    
+    let join (separator: string) (strings: string seq) = String.Join (separator, strings)    
 
-type Packages = JsonProvider<"https://tldr.sh/assets/index.json">
+let [<Literal>] indexJsonUri = "https://tldr.sh/assets/index.json"
 
-let listAllPackages = async {
-    let! packages = Packages.AsyncGetSample()
-    for command in packages.Commands do
+type Packages = JsonProvider<indexJsonUri>
+
+let packageIndex = lazy (Packages.Load indexJsonUri)
+
+let listAllPackages () =
+    for command in packageIndex.Force().Commands do
         match command.Name.String with
         | Some command -> printf "%s, " command
-        | None -> printf "(unknown value: '%A'), " command.Name.JsonValue
+        | None -> printf "(bad json: '%A'), " command.Name.JsonValue
     Console.SetCursorPosition(Console.CursorLeft - 2, Console.CursorTop)
-    printfn "  " }
+    printfn "  "
 
-let listPackage package = async {
-    printfn "listing package: %s" package }
+let listPackage package = printfn "listing package: %s" package
 
 module CLI =
     type CLIArgs =
@@ -36,8 +37,8 @@ module CLI =
     let main args =
         try
             let args = parser.Parse args
-            if args.Contains List_All then Async.RunSynchronously listAllPackages
-            args.TryGetResult <@ Package @> |> Option.iter (Async.RunSynchronously << listPackage)
+            if args.Contains List_All then listAllPackages ()
+            args.TryGetResult <@ Package @> |> Option.iter listPackage
             0
         with
         | :? Argu.ArguParseException as e ->
